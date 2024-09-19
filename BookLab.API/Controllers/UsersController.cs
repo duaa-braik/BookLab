b8bc5f319311805923c2fc6ad6c9c4561fd761f7
duaa-braik <duaa13braik@gmail.com>
2024-09-19 23:09:17 +0300

    Added an endpoint to register a new user

---
+++ BookLab.API/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using BookLab.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLab.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        [HttpPost]
+        [ActionName("SignUp")]
+        public ActionResult SignUp(CreateUserRequest request)
+        {
+            return Ok();
+        }
+    }
+}
