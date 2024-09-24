88da6588cf7f095d0379136828acf78e3e182a4d
duaa-braik <duaa13braik@gmail.com>
2024-09-24 23:40:10 +0300

    Added UsersController class

--- BookLab.API/Controllers/UsersController.cs
+++ BookLab.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using BookLab.API.Dtos;
+using BookLab.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookLab.API.Controllers
@@ -7,13 +8,20 @@ namespace BookLab.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public UsersController(IAuthService authService)
+        {
+            _authService = authService;
+        }
         /// <summary>
         /// Creates a new user.
         /// </summary>
         [HttpPost]
         [ActionName("SignUp")]
-        public ActionResult SignUp(CreateUserRequest request)
-        {
+        public async Task<ActionResult> SignUp(CreateUserRequest request)
+        {            
+            await _authService.CreateUserAsync(request);
             return Ok();
         }
     }
