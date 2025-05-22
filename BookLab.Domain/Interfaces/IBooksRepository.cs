84b504ba40029eed8b45613b58dcb3947828e76a
duaa-braik <duaa13braik@gmail.com>
2025-05-22 22:15:08 +0300

    Added BooksRepository

---
+++ BookLab.Domain/Interfaces/IBooksRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookLab.Domain.Entities;
+
+namespace BookLab.Domain.Repositories
+{
+    public interface IBooksRepository
+    {
+        void CreateBook(Book book);
+    }
+}
---
+++ BookLab.Infrastructure/Repositories/BooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookLab.Domain.Entities;
+using BookLab.Domain.Repositories;
+
+namespace BookLab.Infrastructure.Repositories;
+
+public class BooksRepository : IBooksRepository
+{
+    private readonly BookLabDbContext _context;
+
+    public BooksRepository(BookLabDbContext context)
+    {
+        _context = context;
+    }
+
+    public void CreateBook(Book book)
+    {
+        _context.Book.Add(book);
+    }
+}
