f8826e7d6bef5ce8f6de0f9414f66aceea55590a
duaa-braik <duaa13braik@gmail.com>
2025-10-01 23:34:08 +0300

    Add Book Dto validator class

---
+++ BookLab.Application/Validators/Book/CreateBookValidator.cs
@@ -0,0 +1,29 @@
+﻿using BookLab.Application.Dtos.Book;
+using FluentValidation;
+
+namespace BookLab.Application.Validators.Book;
+
+public class CreateBookValidator : AbstractValidator<CreateBookDto>
+{
+    public CreateBookValidator()
+    {
+        RuleFor(b => b.Title)
+            .NotEmpty().WithMessage("{PropertyName} should not be empty")
+            .NotNull().WithMessage("{PropertyName} should not be null");
+
+        RuleFor(b => b.PublisherId)
+            .NotEmpty().WithMessage("{PropertyName} should not be empty")
+            .NotNull().WithMessage("{PropertyName} should not be null")
+            .GreaterThan(0).WithMessage("{PropertyName} should be a positive number");
+
+        RuleFor(b => b.Price)
+            .NotEmpty().WithMessage("{PropertyName} should not be empty")
+            .NotNull().WithMessage("{PropertyName} should not be null")
+            .GreaterThan(0).WithMessage("{PropertyName} should be a positive number");
+
+        RuleFor(b => b.ISBN)
+            .NotNull().WithMessage("{PropertyName} should not be null")
+            .NotEmpty().WithMessage("{PropertyName} should not be empty")
+            .Length(16).WithMessage("{PropertyName} should be 13 characters");
+    }
+}
